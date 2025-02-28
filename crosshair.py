24c7f39391ac6332ede5c8dbcafe8e037ea26e28
FrostHex <frosts_mage@163.com>
2025-02-28 19:59:04 +0800

    🎨style: 修改代码格式

--- crosshair.py
+++ crosshair.py
@@ -2,36 +2,79 @@ import tkinter as tk  # pip install tkinter
 import pyautogui      # pip install pyautogui
 from math import cos, sin
 
-line_width = 3   # 线粗细（像素）
-line_length = 30 # 线长度（像素）
-gap = 5          # 留空大小（像素）
-threshold = 2    # 鼠标点击的误差范围（像素）
-color = 'light green'  # 十字颜色
+line_wid = 3   # 线粗细（像素）
+line_len = 30  # 线长度（像素）
+line_gap = 5   # 中央留空长度（像素）
+threshold = 2  # 鼠标点击的误差范围（像素）
+line_color = 'light green'  # 十字颜色
 
-cross_type_horizontal = True  # 初始绘制横十字
-theta = 3.1416/4
-diagonal_length = int(line_length / (2 ** 0.5))
-diagonal_gap = int(gap / (2 ** 0.5))
-
-
-def create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal):
-    # 横十字
-    if cross_type_horizontal:
-        canvas.create_line(center_x - line_length, center_y, center_x - gap, center_y, width=line_width, fill=color)  # 左
-        canvas.create_line(center_x + gap, center_y, center_x + line_length, center_y, width=line_width, fill=color)  # 右
-        canvas.create_line(center_x, center_y - line_length, center_x, center_y - gap, width=line_width, fill=color)  # 上
-        canvas.create_line(center_x, center_y + gap, center_x, center_y + line_length, width=line_width, fill=color)  # 下
-    # 斜十字
-    else:
-        canvas.create_line(center_x - diagonal_length, center_y - diagonal_length, center_x - diagonal_gap, center_y - diagonal_gap, width=line_width, fill=color)
-        canvas.create_line(center_x + diagonal_gap, center_y + diagonal_gap, center_x + diagonal_length, center_y + diagonal_length, width=line_width, fill=color)
-        canvas.create_line(center_x - diagonal_length, center_y + diagonal_length, center_x - diagonal_gap, center_y + diagonal_gap, width=line_width, fill=color)
-        canvas.create_line(center_x + diagonal_gap, center_y - diagonal_gap, center_x + diagonal_length, center_y - diagonal_length, width=line_width, fill=color)
+cross_type_horizontal = False  # 绘制横十字或斜十字
+theta = 3.1416/4 # 旋转45度
+line_len_diag = int(line_len / (2 ** 0.5)) # 斜十字线段长度，除以根号2
+line_gap_diag = int(line_gap / (2 ** 0.5)) # 斜十字中央留空长度，除以根号2
+screen_width, screen_height = pyautogui.size()
+center_x = int(screen_width / 2) # 屏幕中心x坐标
+center_y = int(screen_height / 2) # 屏幕中心y坐标
 
 
-def main():
+# @brief: 将鼠标坐标(x, y)绕屏幕中心旋转theta角
+# @param: x, y: 鼠标坐标
+# @param: theta: 旋转角度
+# @return: 旋转后的坐标(x', y')
+def Rotate(x, y, theta):
+    return cos(theta) * (x - center_x) - sin(theta) * (center_y - y) + center_x, sin(theta) * (x - center_x) + cos(theta) * (center_y - y) + center_y
+
+
+# @brief: 更新十字准星
+# @param: canvas: 画布
+# @param: center_x, center_y: 十字准星中心坐标
+# @param: line_len: 十字准星线段长度
+# @param: line_wid: 十字准星线粗细
+# @return: None
+def Cross_Update(canvas, center_x, center_y, line_len, line_wid):
+    global cross_type_horizontal 
+    canvas.delete("all") # 清空画布
+    cross_type_horizontal = not cross_type_horizontal  # 切换十字类型
+    if cross_type_horizontal: # 横十字
+        canvas.create_line(center_x - line_len, center_y, center_x - line_gap, center_y, width=line_wid, fill=line_color)  # 准星左侧线段
+        canvas.create_line(center_x + line_gap, center_y, center_x + line_len, center_y, width=line_wid, fill=line_color)  # 准星右侧线段
+        canvas.create_line(center_x, center_y - line_len, center_x, center_y - line_gap, width=line_wid, fill=line_color)  # 准星上侧线段
+        canvas.create_line(center_x, center_y + line_gap, center_x, center_y + line_len, width=line_wid, fill=line_color)  # 准星下侧线段
+    else: # 斜十字
+        canvas.create_line(center_x - line_len_diag, center_y - line_len_diag, center_x - line_gap_diag, center_y - line_gap_diag, width=line_wid, fill=line_color) # 准星左上线段
+        canvas.create_line(center_x + line_gap_diag, center_y + line_gap_diag, center_x + line_len_diag, center_y + line_len_diag, width=line_wid, fill=line_color) # 准星右下线段
+        canvas.create_line(center_x - line_len_diag, center_y + line_len_diag, center_x - line_gap_diag, center_y + line_gap_diag, width=line_wid, fill=line_color) # 准星左下线段
+        canvas.create_line(center_x + line_gap_diag, center_y - line_gap_diag, center_x + line_len_diag, center_y - line_len_diag, width=line_wid, fill=line_color) # 准星右上线段
+
+
+
+# @brief: 检测鼠标是否在准星范围内
+# @param: canvas: 画布
+# @return: None
+def Cross_Detect(canvas):
+    x, y = pyautogui.position()
     global cross_type_horizontal
-    screen_width, screen_height = pyautogui.size()
+
+    # 若当前是斜十字，则将鼠标坐标旋转45度
+    if not cross_type_horizontal: 
+        x, y = Rotate(x, y, theta)
+
+    # 判断鼠标是否在准星范围内
+    if (x > center_x - line_len - threshold and x < center_x - line_gap + threshold and y > center_y - line_wid//2 - threshold and y < center_y + line_wid//2 + threshold):
+        Cross_Update(canvas, center_x, center_y, line_len, line_wid)
+    elif (x > center_x + line_gap - threshold and x < center_x + line_len + threshold and y > center_y - line_wid//2 - threshold and y < center_y + line_wid//2 + threshold):
+        Cross_Update(canvas, center_x, center_y, line_len, line_wid)
+    elif (x > center_x - line_wid//2 - threshold and x < center_x + line_wid//2 + threshold and y > center_y - line_len - threshold and y < center_y - line_gap + threshold):
+        Cross_Update(canvas, center_x, center_y, line_len, line_wid)
+    elif (x > center_x - line_wid//2 - threshold and x < center_x + line_wid//2 + threshold and y > center_y + line_gap - threshold and y < center_y + line_len + threshold):
+        Cross_Update(canvas, center_x, center_y, line_len, line_wid)
+
+    # 每帧调用一次Cross_Detect，假设帧率为60
+    canvas.after(1000 // 60, lambda: Cross_Detect(canvas)) 
+
+
+# @brief: 主函数
+def main():
     window = tk.Tk()
     window.attributes("-transparentcolor", "white")
     window.attributes("-fullscreen", True)
@@ -40,67 +83,8 @@ def main():
     window.configure(bg='white')
     canvas = tk.Canvas(window, bg='white', highlightthickness=0)
     canvas.pack(fill=tk.BOTH, expand=tk.YES)
-
-    center_x = int(screen_width / 2)
-    center_y = int(screen_height / 2)
-
-    def rotate(x, y, theta):
-        return cos(theta) * (x - center_x) - sin(theta) * (center_y - y) + center_x, sin(theta) * (x - center_x) + cos(theta) * (center_y - y) + center_y
-
-
-    def update_cross():
-        global cross_type_horizontal
-        x, y = pyautogui.position()
-        # 若当前是斜十字，则对鼠标位置进行旋转
-        if not cross_type_horizontal:
-            x, y = rotate(x, y, theta)
-
-        if (x > center_x - line_length - threshold and x < center_x - gap + threshold and
-            y > center_y - line_width//2 - threshold and y < center_y + line_width//2 + threshold):
-                canvas.delete("all")
-                cross_type_horizontal = not cross_type_horizontal  # 切换十字类型
-                create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal)
-        elif (x > center_x + gap - threshold and x < center_x + line_length + threshold and
-              y > center_y - line_width//2 - threshold and y < center_y + line_width//2 + threshold):
-                canvas.delete("all")
-                cross_type_horizontal = not cross_type_horizontal
-                create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal)
-        elif (x > center_x - line_width//2 - threshold and x < center_x + line_width//2 + threshold and
-              y > center_y - line_length - threshold and y < center_y - gap + threshold):
-                canvas.delete("all")
-                cross_type_horizontal = not cross_type_horizontal
-                create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal)
-        elif (x > center_x - line_width//2 - threshold and x < center_x + line_width//2 + threshold and
-              y > center_y + gap - threshold and y < center_y + line_length + threshold):
-                canvas.delete("all")
-                cross_type_horizontal = not cross_type_horizontal
-                create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal)
-
-        canvas.after(1000 // 600, update_cross)  # 每帧调用一次update_cross，假设帧率为60
-
-
-    # x_test = 0
-    # y_test = 0
-    # cross_type_horizontal = False
-    # for x_raw in range(center_x - 100, center_x + 100):
-    #     for y_raw in range(center_y - 100, center_y + 100):
-    #         x_test, y_test = rotate(x_raw, y_raw, theta)
-    #         if (x_test > center_x - line_length - threshold and x_test < center_x - gap + threshold and
-    #             y_test > center_y - line_width//2 - threshold and y_test < center_y + line_width//2 + threshold):
-    #             canvas.create_line(x_raw-1, y_raw-1, x_raw+1, y_raw+1, fill='light blue')
-    #         elif (x_test > center_x + gap - threshold and x_test < center_x + line_length + threshold and
-    #             y_test > center_y - line_width//2 - threshold and y_test < center_y + line_width//2 + threshold):
-    #             canvas.create_line(x_raw-1, y_raw-1, x_raw+1, y_raw+1, fill='light blue')
-    #         elif (x_test > center_x - line_width//2 - threshold and x_test < center_x + line_width//2 + threshold and
-    #             y_test > center_y - line_length - threshold and y_test < center_y - gap + threshold):
-    #             canvas.create_line(x_raw-1, y_raw-1, x_raw+1, y_raw+1, fill='light blue')
-    #         elif (x_test > center_x - line_width//2 - threshold and x_test < center_x + line_width//2 + threshold and
-    #             y_test > center_y + gap - threshold and y_test < center_y + line_length + threshold):
-    #             canvas.create_line(x_raw-1, y_raw-1, x_raw+1, y_raw+1, fill='light blue')
-
-    # 创建初始横十字
-    create_cross(canvas, center_x, center_y, line_length, line_width, cross_type_horizontal)
-    update_cross()
+    Cross_Update(canvas, center_x, center_y, line_len, line_wid) # 创建准星
+    Cross_Detect(canvas)
     window.mainloop()
 
 
